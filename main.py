15c593ff044669f44ebfbe9a02c49e6f78d3c6fd
p1x3lx01 <148356413+p1x3lx01@users.noreply.github.com>
2023-10-18 20:53:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -17,18 +17,22 @@ print("""
 site = input("Enter your Blog Address: ")
 blog = int(input("Enter The number of Viewers: "))
 
+search_engines = ['https://www.google.com/', 'https://www.bing.com/', 'https://search.yahoo.com/']  # قائمة بمحركات البحث
+
 def run():
-    # تحديث رأس User-Agent بشكل عشوائي مع كل زيارة
+    search_engine = random.choice(search_engines)
+    
+    # تحديث رأس User-Agent و Referer بشكل عشوائي مع كل زيارة
     headers = {
         "User-Agent": ua.random,
-        "Referer": f"https://www.google.com/search?q={random.choice(['twitter', '@ahmed629231', '1714679426133201174'])}"
+        "Referer": search_engine
     }
     
     response = requests.get(site, headers=headers, proxies=proxies, verify=False)
     print("[" + str(i) + "]" + " Blog View Added With IP:" + requests.get('http://ipecho.net/plain', proxies=proxies, headers=headers).text)
     
-    # انتظر فترة زمنية عشوائية بين 5 إلى 10 ثوانٍ بين الطلبات لجعلها تبدو أكثر واقعية
-    time.sleep(random.uniform(5, 10))
+    # انتظر فترة زمنية عشوائية بين 5 إلى 20 ثانية بين الطلبات لجعلها تبدو أكثر واقعية
+    time.sleep(random.uniform(5, 20))
 
 if __name__ == '__main__':
     for i in range(blog):
